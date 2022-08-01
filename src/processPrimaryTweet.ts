c96f4cd7dde4057e0701cc558787293d880ac930
Ray Bun <ray@nakz.net>
2022-08-01 21:20:39 +1000

    added bitcoin

--- src/processPrimaryTweet.ts
+++ src/processPrimaryTweet.ts
@@ -3,8 +3,8 @@ export async function processPrimaryTweet(userTargetTweet: any, requestedUserTwi
     try {
       const tweetID: string = userTargetTweet.id_str;
       const tweetText: string = userTargetTweet.full_text;
-      const tweetTextWithoutURL: string = tweetText.replace(/https?:\/\/\S+/g, "");
-      const addBitcoinToTweet: string = tweetTextWithoutURL.replace(/#[Bb][iI][tT][cC][oO][Ii][nN]/g, "#Bitcoin *₿*");
+      const addBitcoinToTweet: string = tweetText.replace(/#[Bb][iI][tT][cC][oO][Ii][nN]/g, "#Bitcoin *₿*");
+      const tweetTextWithoutURL: string = addBitcoinToTweet.replace(/https?:\/\/\S+/g, "");
       const tweetAuthorId: string = userTargetTweet.user.id_str;
       const tweetUserName: string = userTargetTweet.user.screen_name;
       const tweetName: string = userTargetTweet.user.name;
@@ -15,7 +15,7 @@ export async function processPrimaryTweet(userTargetTweet: any, requestedUserTwi
       const statusURL: string = `https://twitter.com/${tweetUserName}/status/${tweetID}`;
       let tweetMediaFile = "media" in userTargetTweet.entities ? userTargetTweet.entities.media[0].media_url_https : null;
       // Insert tweeted request into database
-      resolve({ tweetID, addBitcoinToTweet, tweetAuthorId, tweetUserName, tweetName, tweetVerified, tweetProfileImageUrl, tweetMediaFile, tweetUserId, statusURL });
+      resolve({ tweetID, tweetTextWithoutURL, tweetAuthorId, tweetUserName, tweetName, tweetVerified, tweetProfileImageUrl, tweetMediaFile, tweetUserId, statusURL });
     } catch (err) {
       console.log("Error catched at processPrimaryTweet.ts: ", err);
       reject(err);
